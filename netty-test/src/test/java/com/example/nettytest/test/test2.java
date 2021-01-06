39a2ce8ff46cb03f2c16f3e8e825161253487847
yin <yinjingem@163.com>
2021-01-06 17:44:17 +0800

    Signed-off-by: yin <yinjingem@163.com>

---
+++ netty-test/src/test/java/com/example/nettytest/test/test2.java
@@ -0,0 +1,17 @@
+package com.example.nettytest.test;
+
+/**
+ * @Package: com.example.nettytest.test
+ * @ClassName: test2
+ * @Author: yinj
+ * @CreateTime: 2021/1/6 17:06
+ * @Description: TODO
+ */
+public class test2 {
+
+    public static void main(String[] args) {
+        System.out.println("测试一下");
+    }
+
+
+}
